140d423559827aa3d25ec044e060172043d40d08
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2022-08-19 09:28:22 +0200

    Repro mask bug

--- src/App.hx
+++ src/App.hx
@@ -1,10 +1,5 @@
 import h2d.Bitmap;
 import h2d.filter.Mask;
-import h2d.Graphics;
-import hxd.res.DefaultFont;
-import h2d.Text;
-import h2d.Tile;
-import h2d.Flow;
 
 class App extends hxd.App {
 	static function main() {
@@ -16,27 +11,24 @@ class App extends hxd.App {
 	override function init() {
 		hxd.Res.initEmbed();
 
+		engine.backgroundColor = 0xff0000;
+
 		final shapeTile = hxd.Res.shape.toTile();
 		shapeTile.setCenterRatio();
 		shape = new Bitmap(shapeTile, s2d);
 		shape.x = s2d.width / 2;
 		shape.y = s2d.height / 2;
 
-		final flow = new Flow(s2d);
-		flow.backgroundTile = Tile.fromColor(0xff0000);
-		flow.fillHeight = true;
-		flow.fillWidth = true;
-		flow.verticalAlign = Middle;
-		flow.horizontalAlign = Middle;
-		// The second argument is being ignored?
-		flow.filter = new Mask(shape, true, true);
-
-		final text = new Text(hxd.res.DefaultFont.get(), flow);
-		text.text = "Hello, is this a mask bug?";
-		text.scale(5);
+		final shipTile = hxd.Res.ship.toTile();
+		shipTile.setCenterRatio();
+		final ship = new Bitmap(shipTile, s2d);
+		ship.width = s2d.width;
+		ship.x = s2d.width / 2;
+		ship.y = s2d.height / 2;
+		ship.filter = new Mask(shape, true, false);
 	}
 
 	override function update(dt:Float) {
-		shape.rotation += dt * 1.0;
+		shape.rotation += dt * 0.5;
 	}
 }
